ac4e079db96cd6ae6f67e54982873e59219f1aec
KatherineMelnyk <31983901+KatherineMelnyk@users.noreply.github.com>
2017-11-27 21:10:40 +0200

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -71,7 +71,20 @@ bool operator==(const BigNumber& left, const BigNumber&right) {
 		return true;
 }
 
-const BigNumber operator + ( BigNumber &left, const BigNumber &right) {
+bool operator<(const BigNumber& left, const BigNumber&right) {
+	if (left._number.size() != right._number.size()) {
+		return left._number.size() < right._number.size();
+	}
+	else {
+		for (long long i = left._number.size(); i >= 0; --i) {
+			if (left._number[i] != right._number[i])
+				return left._number[i] < right._number[i];
+		}
+		return false;
+	}
+}
+
+const BigNumber operator + (BigNumber &left,BigNumber &right) {
 	int carry = 0;
 	for (size_t i = 0; i < std::max(left._number.size(), right._number.size()); ++i) {
 		if (left._number.size() == i)
@@ -84,11 +97,136 @@ const BigNumber operator + ( BigNumber &left, const BigNumber &right) {
 	return left;
 }
 
+const BigNumber BigNumber::operator +() const {
+	return BigNumber(*this);
+}
+
+const BigNumber BigNumber::operator-() const {
+	BigNumber copy(*this);
+	return copy;
+}
+
+bool operator !=(const BigNumber& left, const BigNumber& right) {
+	return !(left == right);
+}
+
+bool operator <=(const BigNumber& left, const BigNumber& right) {
+	return (left < right || left == right);
+}
+
+bool operator >(const BigNumber& left, const BigNumber& right) {
+	return !(left <= right);
+}
+
+bool operator >=(const BigNumber& left, const BigNumber& right) {
+	return !(left < right);
+}
+
+BigNumber& BigNumber::operator +=(BigNumber value) {
+	return *this = (*this + value);
+}
+
+BigNumber& BigNumber::operator -=(BigNumber value) {
+	return *this = (*this - value);
+}
+
+BigNumber& BigNumber::operator *=(BigNumber value) {
+	return *this = (*this * value);
+}
+
+const BigNumber operator -(BigNumber &left,BigNumber &right) {
+	if (left == right) {
+		for (size_t i = 0; i < right._number.size(); ++i) {
+			left._number[i]=0;
+		}
+	}
+	left.remove_leading_zeroes();
+	return left;
+	if (left > right) {
+		int carry = 0;
+		for (size_t i = 0; i < right._number.size(); ++i) {
+			left._number[i] -= (carry + (i < right._number.size() ? right._number[i] : 0));
+			carry = left._number[i] < 0;
+			if (carry != 0)
+				left._number[i] += BASE;
+		}
+	}
+	left.remove_leading_zeroes();
+	return left;
+}
+
+const BigNumber operator *(BigNumber &left, BigNumber &right) {
+	BigNumber result;
+	result._number.resize(left._number.size() + right._number.size());
+	for (size_t i = 0; i < left._number.size(); ++i) {
+		int carry = 0;
+		for (size_t j = 0; j < right._number.size() || carry != 0; ++j) {
+			long long current = result._number[i + j] + left._number[i] * 1LL * (j < right._number.size() ? right._number[j] : 0) + carry;
+			result._number[i + j] = static_cast<int>(current%BASE);
+			carry = static_cast<int>(current / BASE);
+		}
+	}
+	result.remove_leading_zeroes();
+	return result;
+}
+
+BigNumber BigNumber::Karatsuba_mul(BigNumber& left, BigNumber& right) {
+	auto len = max(left._number.size(), right._number.size());
+	if (len < MIN_FOR_KARATSUBA) {
+		return left*right;
+	}
+	BigNumber result;
+	result._number.resize(len*2+1);
+	auto k = len/2;
+	BigNumber left_1, left_2;
+	left_1._number.resize((len + 1) / 2);
+	for (size_t i = 0; i < (len + 1)/2; ++i) {
+		left_1._number[i] = left._number[i];
+	}
+	left_2._number.resize((len + 1) / 2);
+	for (size_t i = 0; i<len/2; ++i) {
+		left_2._number[i] = left._number[i + len / 2 + 1];
+	}
+	left_2.remove_leading_zeroes();
+	BigNumber right_1, right_2;
+	right_1._number.resize((len + 1) / 2);
+	for (size_t i = 0; i < (len + 1) / 2; ++i) {
+		right_1._number[i] = right._number[i];
+	}
+	right_2._number.resize((len + 1) / 2);
+	for (size_t i = 0; i < len / 2; ++i) {
+		right_2._number[i] = right._number[i + len / 2 + 1];
+	}
+	right_2.remove_leading_zeroes();
+	BigNumber Num_1 = Karatsuba_mul(left_1, right_1);
+	BigNumber Num_2 = Karatsuba_mul(left_2, right_2);
+	BigNumber left_1_result, right_1_result;
+	left_1_result._number.resize(k);
+	right_1_result._number.resize(k);
+	for (size_t i = 0; i < k; ++i) {
+		BigNumber a = left_1 + left_2;
+		BigNumber b = right_1 + right_2;
+		left_1_result._number[i] = a._number[i];
+		right_1_result._number[i] = b._number[i];
+	}
+	left_1_result.remove_leading_zeroes();
+	right_1_result.remove_leading_zeroes();
+	BigNumber Num_3 = Karatsuba_mul(left_1_result, right_1_result);
+...
+}
+
+
+
 int main() {
-	BigNumber BG("123456789");
-	BigNumber BN("123456789");
-	cout << BG << endl;
-	cout << BG + BN << endl;
+	BigNumber a("123456789129128818");
+	BigNumber b("123456789129128818");
+	BigNumber c("123456789");
+	BigNumber d("123456789");
+	cout << a << endl;
+	cout << a + c << endl;
+	cout << b - d << endl;
+	//cout << c - d << endl;
+	//cout << c * d << endl;
 	system("pause");
 	return 0;
 }
