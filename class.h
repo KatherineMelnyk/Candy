1c8d3090db4fbc9e004bcea69616cb003fd1f7f0
KatherineMelnyk <31983901+KatherineMelnyk@users.noreply.github.com>
2017-11-29 17:41:35 +0200

    Update class.h

--- class.h
+++ class.h
@@ -17,12 +17,6 @@ public:
 	friend std::ostream& operator<<(std::ostream& os, const BigNumber& BG);
 	operator std::string() const;
 	
-	BigNumber& operator -=(BigNumber value);
-	BigNumber& operator +=(BigNumber value);
-	BigNumber& operator *=(BigNumber value);
-	BigNumber& operator*= (int n);
-	BigNumber& operator/= (int n);
-	
 	friend bool operator==(const BigNumber& left, const BigNumber&right);
 	friend bool operator<(const BigNumber& left, const BigNumber&right);
 	friend bool operator !=(const BigNumber& left, const BigNumber& right);
@@ -30,12 +24,22 @@ public:
 	friend bool operator >(const BigNumber& left, const BigNumber& right);
 	friend bool operator >=(const BigNumber& left, const BigNumber& right);
 	
-	friend BigNumber operator + (BigNumber &left, BigNumber &right);
-	friend BigNumber operator - (BigNumber &left, BigNumber &right);
-	friend BigNumber operator * (BigNumber &left, BigNumber &right);
 	friend BigNumber operator * (BigNumber &left, int &n);
 	friend BigNumber operator / (BigNumber &left, int &n);
 	friend int operator % (BigNumber &left, int &n);
+	
+	friend BigNumber operator + (BigNumber &left, BigNumber &right);
+	friend BigNumber operator - (BigNumber &left, BigNumber &right);
+	friend BigNumber operator * (BigNumber &left, BigNumber &right);
+	
+	BigNumber& operator -=(BigNumber value);
+	BigNumber& operator +=(BigNumber value);
+	BigNumber& operator *=(BigNumber value);
+	BigNumber& operator*= (int n);
+	BigNumber& operator/= (int n);
+	
+	BigNumber operator --();
+	BigNumber operator ++();
 	
 	BigNumber pow_(BigNumber &left);
 	BigNumber Karatsuba_mul(BigNumber& left, BigNumber& right);
